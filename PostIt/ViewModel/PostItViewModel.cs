e9c3455183b2c12d78caed30086b38fd1650ed62
Blindev <f.barre@hotmail.ch>
2020-11-03 14:48:15 +0100

    Double click to edit post-it

--- PostIt/ViewModel/PostItViewModel.cs
+++ PostIt/ViewModel/PostItViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using PostIt.Database;
 using PostIt.Model;
@@ -11,15 +12,26 @@ namespace PostIt.ViewModel
 {
     public class PostItViewModel : BaseViewModel
     {
+        public static event EventHandler ElementAreFocused;
+
         public PostItViewModel(Model.PostIt postIt)
         {
             Model = postIt;
+            ElementAreFocused += PostItViewModel_ElementAreFocused;
+        }
+
+        private void PostItViewModel_ElementAreFocused(object sender, EventArgs e)
+        {
+            if (!sender.Equals(this))
+            {
+                IsNotEditable = true;
+            }
         }
 
         public Model.PostIt Model { get; protected set; }
 
-        public string Text 
-        { 
+        public string Text
+        {
             get => Model.Text;
             set
             {
@@ -32,6 +44,41 @@ namespace PostIt.ViewModel
         private void UpdatePostIt()
         {
             PostItContext.UpdatePostIt(Model);
+        }
+
+        private bool _isNotEditable = true;
+        public bool IsNotEditable
+        {
+            get => _isNotEditable;
+            protected set
+            {
+                _isNotEditable = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsEditable));
+            }
+        }
+        public bool IsEditable => !IsNotEditable;
+
+
+        private ICommand _mouseDoubleClick;
+        public ICommand MouseDoubleClick => _mouseDoubleClick ??= new CommandHandler(MouseDoubleClickAction, true);
+        private void MouseDoubleClickAction(object parameter)
+        {
+            IsNotEditable = false;
+            ElementAreFocused?.Invoke(this, EventArgs.Empty);
+            if (parameter is TextBox textBox)
+            {
+                textBox.Focus();
+                textBox.Select(textBox.Text.Length, 0);
+            }
+        }
+
+
+        private ICommand _loseFocus;
+        public ICommand LoseFocus => _loseFocus ??= new CommandHandler(LoseFocusAction, true);
+        private void LoseFocusAction(object parameter)
+        {
+            IsNotEditable = true;
         }
     }
 }
